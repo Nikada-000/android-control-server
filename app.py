56efd1e2446278caaa8a4c1b0f91d220a99b08da
Nikada-000 <mewtoon280@gmail.com>
2025-06-18 20:14:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -98,5 +98,3 @@ def video_stream():
 
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
